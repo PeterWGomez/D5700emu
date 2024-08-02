459db6faa22e4841534c87eac3f93fefa426a403
PeterWGomez <petergomezcs@gmail.com>
2024-08-01 23:00:39 -0600

    Refactored store and draw

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,12 @@ package org.example
 
 fun main() {
     var input = ""
-    do {
+    println("Enter rom name (enter exit to close):")
+    input = readLine().toString()
+    while (input != "exit") {
+        val emulatorInstance = emulator()
+        emulatorInstance.runEmulator(input)
         println("Enter rom name (enter exit to close):")
         input = readLine().toString()
-        val emulatorInstance = emulator(0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, )
-        emulatorInstance.runEmulator()
-    } while (input != "exit")
+    }
 }
--- src/main/kotlin/emulator.kt
+++ src/main/kotlin/emulator.kt
@@ -2,36 +2,28 @@ package org.example
 
 import java.io.File
 
+@OptIn(ExperimentalUnsignedTypes::class)
 class emulator(
     // General purpose registers
-    var r0: UByte,
-    var r1: UByte,
-    var r2: UByte,
-    var r3: UByte,
-    var r4: UByte,
-    var r5: UByte,
-    var r6: UByte,
-    var r7: UByte,
+    var generalRegisters: UByteArray = UByteArray(8),
     // Special registers
-    var P: UShort,
-    var T: UByte,
-    var A: UShort,
-    var M: UByte,     // There is no bit in Kotlin, using this
+    var P: UShort = 0u,
+    var T: UByte = 0u,
+    var A: UShort = 0u,
+    var M: UByte = 0u,     // There is no bit in Kotlin, using this
     // Screen
-    var byteArray: ByteArray = ByteArray(64)
+    var screenArray: ByteArray = ByteArray(64)
 ) {
     init {
-        byteArray.fill(0)
+        screenArray.fill(0)
     }
 
-    fun runEmulator() {
+    fun runEmulator(input: String) {
         // Read the ROM
-        val fileName = "roms/hello.d5700"
+        // Need error checking on input
+        val fileName = "roms/${input}.d5700"
         val lines: List<String> = File(fileName).readLines()
         for (line in lines) {
-            // Execute each line
-            // Test to see command
-            //println(line)
             // Run STORE
             if (line[0] == '0'){
                 STORE(line[1], line[2].toString()+line[3].toString())
@@ -44,82 +36,24 @@ class emulator(
     }
 
     fun STORE(rX: Char, bb: String) {
-        println("Running Store")
         // Assign register the value
-        if (rX == '0') {
-            r0 = bb.toUByte(16)
-            //println(r0)
-            //test
-            //println(r0.toByte().toChar())
-        }
-        if (rX == '1') {
-            r1 = bb.toUByte(16)
-            //println(r1)
-        }
-        if (rX == '2') {
-            r2 = bb.toUByte(16)
-            //println(r2)
-        }
-        if (rX == '3') {
-            r3 = bb.toUByte(16)
-            //println(r3)
-        }
-        if (rX == '4') {
-            r4 = bb.toUByte(16)
-            //println(r4)
-        }
-        if (rX == '5') {
-            r5 = bb.toUByte(16)
-            //println(r5)
-        }
-        if (rX == '6') {
-            r6 = bb.toUByte(16)
-            //println(r6)
-        }
-        if (rX == '7') {
-            r7 = bb.toUByte(16)
-            //println(r7)
-        }
+        generalRegisters[rX.toString().toInt()] = bb.toUByte(16)
     }
     fun DRAW(rX: Char, rY: UByte, rZ: UByte) {
         // rX is the register being read. rY is the row, rZ is the column of where it will be printed
-        //println("Running DRAW")
-        // Assign register the value
-        if (rX == '0') {
-            byteArray[(rY.toInt()+1)*(rZ.toInt()+1)-1] = r0.toByte()
-        }
-        if (rX == '1') {
-            byteArray[(rY.toInt()+1)*(rZ.toInt()+1)-1] = r1.toByte()
-        }
-        if (rX == '2') {
-            byteArray[(rY.toInt()+1)*(rZ.toInt()+1)-1] = r2.toByte()
-        }
-        if (rX == '3') {
-            byteArray[(rY.toInt()+1)*(rZ.toInt()+1)-1] = r3.toByte()
-        }
-        if (rX == '4') {
-            byteArray[(rY.toInt()+1)*(rZ.toInt()+1)-1] = r4.toByte()
-        }
-        if (rX == '5') {
-            byteArray[(rY.toInt()+1)*(rZ.toInt()+1)-1] = r5.toByte()
-        }
-        if (rX == '6') {
-            byteArray[(rY.toInt()+1)*(rZ.toInt()+1)-1] = r6.toByte()
-        }
-        if (rX == '7') {
-            byteArray[(rY.toInt()+1)*(rZ.toInt()+1)-1] = r7.toByte()
-        }
+            screenArray[(rY.toInt()+1)*(rZ.toInt()+1)-1] = generalRegisters[rX.toString().toInt()].toByte()
 
         // Draw the current screen
         println("========")
         var charCounter = 0
-        for (i in byteArray) {
+        for (i in screenArray) {
             print(i.toChar())
             charCounter++
             if (charCounter % 8 == 0){
                 println()
             }
         }
+        println("========")
     }
 
 }
